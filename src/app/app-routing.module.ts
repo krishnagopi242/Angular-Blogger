48c85a4a922289bc78ad21cf9d54c2c62b414c05
unknown <krishnagopi242@gmail.com>
2021-02-19 20:02:51 +0530

    login and home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'Login',
+    component: LoginComponent,
+  },
+  {
+    path: 'Home',
+    component: HomeComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/Login',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
